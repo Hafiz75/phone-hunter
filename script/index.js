2f7ddac141d56b78c25a2b18362c9de46655aed9
Hafiz75 <hafizrmn7@gmail.com>
2024-02-26 00:17:45 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,108 @@
+ /* this parameter's argument comes from handleShowAll() */
+async function loadData(searchInp,showAll) {
+    /* show spinner */
+    document.getElementById('spinner').classList.remove('hidden')
+    /* show overlay */
+    document.getElementById('overlay').classList.add('opacity-20')
+    /* this condition work when search input is empty or undefined */
+    if (searchInp == undefined || searchInp == '') {
+        searchInp = 'a'
+    }
+    let asset = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchInp}`);
+    let phoneObj = await asset.json()
+    displayData(phoneObj,showAll)
+}
+   /* loadData also called in whatSearch() function.this whatSearch() trigger all function.so when we not press the search button this page will be blank.to prevent this blankness loadData() function called here and this function called other function synchronously */
+   loadData()
+   /* this parameter's argument comes from handleShowAll() */
+function whatSearch(showAll) {
+    let searchInp = document.getElementById('whatSearch').value
+    loadData(searchInp,showAll)
+}
+function handleShowAll() {
+    let showAll = true
+    whatSearch(showAll)
+}
+
+function displayData(some,showAll) {
+    /* select show all button */
+    let showAllBtn = document.getElementById('showAll')
+    let phoneContainer = document.getElementById('phoneContainer')
+    /* remove previous data */
+    phoneContainer.textContent = ''
+    let phonesArray = some.data
+    if (showAll == true) {
+        phonesArray = phonesArray;
+        showAllBtn.classList.add('hidden')
+    }else{
+        phonesArray = phonesArray.slice(0,15)
+        showAllBtn.classList.remove('hidden')
+    }
+
+    for (const items of phonesArray) {
+        phoneContainer.innerHTML += 
+        `
+        <div class="card border border-[#CFCFCF] p-4 md:p-6">
+        <figure class="p-5 md:p-10 bg-[rgba(13,110,253,0.05)]">
+          <img src="${items.image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center p-1">
+          <h3 class="text-xl md:text-2xl text-[#403F3F] font-bold">${items.phone_name}</h3>
+          <p class="text-[#706F6F] md:text-lg">There are many variations of passages of available, but the majority have suffered</p>
+          <h3 class="text-xl md:text-2xl text-[#403F3F] font-bold">$999</h3>
+          <div class="card-actions">
+            <button class="text-lg md:text-xl text-[#FFF] font-semibold bg-[#0D6EFD] px-4 py-2 md:px-6 rounded-lg" onclick="handleShowDetails('${items.slug}'); my_modal_1.showModal()">Show Details</button>
+          </div>
+        </div>
+      </div>
+        `
+    }
+  /* hidden spinner bcz data loading is finished */
+  document.getElementById('spinner').classList.add('hidden')
+  /* remove overlay  */
+  document.getElementById('overlay').classList.remove('opacity-20')
+}
+
+async function handleShowDetails(id) {
+    let asset = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    let phoneObj = await asset.json()
+    disShowDetails(phoneObj)
+}
+
+function disShowDetails(some) {
+    let data = some.data;
+    let dialog = document.getElementById('my_modal_1')
+    /* create and push rest of modal element */
+    dialog.innerHTML = 
+    `<div class="modal-box p-5">
+    <div class="bg-[rgba(13,110,253,0.05)] rounded-lg">
+        <img class="m-auto" src="${data.image}" alt="">
+    </div>
+    <h3 class="font-bold text-xl md:text-2xl text-[#403F3F]">${data.name}</h3>
+    <p class="pt-4 text-sm md:text-base text-[#706F6F]">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+    <!-- feature container -->
+    <div>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">Storage :</span><span class="text-[#706F6F] text-sm md:text-base"> ${data.mainFeatures.storage} </span></p>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">Display Size :</span><span class="text-[#706F6F] text-sm md:text-base"> ${data.mainFeatures.displaySize} </span></p>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">Chipset :</span><span class="text-[#706F6F] text-sm md:text-base"> ${data.mainFeatures.chipSet} </span></p>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">Memory :</span><span class="text-[#706F6F] text-sm md:text-base"> ${data.mainFeatures.memory} </span></p>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">Release Date :</span><span class="text-[#706F6F] text-sm md:text-base"> ${data.releaseDate} </span></p>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">Brand :</span><span class="text-[#706F6F] text-sm md:text-base"> ${data.brand} </span></p>
+        <p><span class="text-[#403F3F] text-sm md:text-base font-semibold">GPS :</span><span class="text-[#706F6F] text-sm md:text-base"> Yes </span></p>
+    </div>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn bg-[#DC3545] text-white">Close</button>
+      </form>
+    </div>
+  </div>`
+}
+
+/* banner image showing control */
+
+if (window.innerWidth < 500) {
+    document.getElementById('bannerImg1').classList.add('hidden')
+    document.getElementById('bannerImg3').classList.add('hidden')
+}
+
